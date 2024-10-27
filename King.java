d939762b6b34ca8e1abca62aed7a64db971a4a09
AmoghKhaparde <122950756+AmoghKhaparde@users.noreply.github.com>
2024-10-27 15:39:12 -0700

    Create King.java

---
+++ King.java
@@ -0,0 +1,34 @@
+public class King extends Piece {
+    public King(int row, int col, String color) {
+        super(row, col, color, "King"); // makes sure to communicate with the super class about row, column and color and type of piece
+    }
+
+    public boolean isValidMove(int[] end, Piece[][] board){
+        String color = super.getColor();
+        int[] start = super.getCoords();
+        
+        int rowDiff = Math.abs(start[0] - end[0]);
+        int colDiff = Math.abs(start[1] - end[1]);
+        
+        boolean isOneSquareMove;
+        
+        //checks if the king is moving only one square
+        //rowDiff and colDiff can be 1 or less but if they are both 0 then the king isn't moving any squares which isn't allowed
+        if (rowDiff <= 1 && colDiff <=1 && (rowDiff != 0 || colDiff != 0)){
+            isOneSquareMove = true;
+        } else {
+            isOneSquareMove = false;
+        }
+        
+        if (!isOneSquareMove){ // kings can only move one square
+            return false; 
+        }
+        
+        
+        
+        // if the destination is empty or the opposite color it is valid
+        return (board[end[0]][end[1]] == null || board[end[0]][end[1]].getColor() != getColor());
+        
+        
+    }
+}
